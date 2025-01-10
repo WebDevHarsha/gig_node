297ce3419be2561eb4fef33791df5e783c98091d
WebDevHarsha <vasriharsha@gmail.com>
2025-01-10 10:54:44 +0530

    home page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import HomePage from '@/components/Home'
 import { Navbar } from '@/components/Navbar'
 import React from 'react'
 
 function page() {
   return (
+    <>
     <Navbar></Navbar>
+    <HomePage></HomePage>
+    </>
   )
 }
 
